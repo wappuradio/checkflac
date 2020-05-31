261d8f015b42776568f89cb5a9d0854890d53625
Ossi Hakkarainen <ossi@hakkarainen.net>
2020-05-31 21:28:25 +0300

    Fix total discs

--- irclevyt.sh
+++ irclevyt.sh
@@ -6,9 +6,7 @@ tail -f "/home/wappuradio/irc/192.98.101.230/#radiontoimitus/out" | \
         if [[ "$mesg" =~ $regex ]]; then
           dbcount=$(awk '{print $1}' < /home/flac/dbcount.txt)
           hddspace=$(df -h|grep upload|awk '{print $3 "/" $2}')
-          finddirs=$(find /home/wappuradio/db -type d 2>/dev/null|wc -l)
-          discs=0
-          let "discs = $dbcount + ($finddirs - $dbcount)"
+          discs=$(find /home/wappuradio/db -type d -not -name CD1 2>/dev/null|wc -l)
           echo "/notice #radiontoimitus :$dbcount albums, $discs discs, hdd space $hddspace"
         fi
     done > "/home/wappuradio/irc/192.98.101.230/#radiontoimitus/in"
